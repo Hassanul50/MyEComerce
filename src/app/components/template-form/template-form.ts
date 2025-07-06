2e5b9f1a3ef14cb6e126b2a533a9385f9428c258
Md Hassanul Banna <44776893+Hassanul50@users.noreply.github.com>
2025-07-06 17:10:38 +0600

    template from work

--- src/app/components/template-form/template-form.ts
+++ src/app/components/template-form/template-form.ts
@@ -1,11 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-form',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './template-form.html',
   styleUrl: './template-form.css'
 })
 export class TemplateForm {
+  
+  userObj: any={
+    firsrName :'',
+    lastName : '',
+    userName : '',
+    city: '',
+    state:'',
+    zipcode :'',
+    isTermAgree:false
+  }
+
+  onSave(){
+   
+    debugger;
+    const formValue=this.userObj;
+  }
 
 }
